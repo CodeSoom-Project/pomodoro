6122ecb7a176c4252f4885b3e87a4ed3cf5f9901
Woo Dy <kjop7419@naver.com>
2022-08-03 12:47:44 +0900

    feat: 프로젝트 기본 세팅

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Global } from '@emotion/react';
+
+import reset from 'styles/reset';
+
+function App() {
+  return (
+    <>
+      <Global styles={reset} />
+      <h1>포모도로 시간 관리</h1>
+    </>
+  );
+}
+
+export default App;
