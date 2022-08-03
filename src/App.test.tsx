558b650d33912cfe25d154a76204f2d8b6711bb0
Woo Dy <kjop7419@naver.com>
2022-08-03 16:36:00 +0900

    style: 헤더를 삭제하였습니다.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,9 +3,10 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  it('헤더 문구가 보여집니다.', () => {
+  it('버튼이 보여집니다.', () => {
     const { getByText } = render(<App />);
 
-    expect(getByText('포모도로 시간 관리')).not.toBeNull();
+    expect(getByText('Focus')).not.toBeNull();
+    expect(getByText('Break')).not.toBeNull();
   });
 });
