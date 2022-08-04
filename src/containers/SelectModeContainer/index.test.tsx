a091fd3c8368410449a9e4eabbf5c361cbd49f8f
Woo Dy <kjop7419@naver.com>
2022-08-04 14:42:05 +0900

    test: naviagte 테스트를 추가하였습니다.

--- src/containers/SelectModeContainer/index.test.tsx
+++ src/containers/SelectModeContainer/index.test.tsx
@@ -1,11 +1,36 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
 import SelectModeContainer from '.';
 
+const mockedUsedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUsedNavigate,
+}));
+
 describe('SelectModeContainer', () => {
+  const renderSelectModeContainer = () => {
+    return render(
+      <MemoryRouter>
+        <SelectModeContainer />
+      </MemoryRouter>
+    );
+  };
+
   it('버튼이 렌더링됩니다.', () => {
-    const { getByText } = render(<SelectModeContainer />);
+    const { getByText } = renderSelectModeContainer();
 
     expect(getByText('Focus')).not.toBeNull();
     expect(getByText('Break')).not.toBeNull();
+  });
+
+  it('Foucs로 이동합니다', () => {
+    const { getByText } = renderSelectModeContainer();
+
+    fireEvent.click(getByText('Focus'));
+
+    expect(mockedUsedNavigate).toHaveBeenCalledTimes(1);
   });
 });
