ff645c6860a2b8acd9ebd8f08892c2c6b8a48610
Woo Dy <kjop7419@naver.com>
2022-08-04 14:41:26 +0900

    test: Route연결로 인해 테스트를 수정하였습니다

--- src/pages/SelectMode/index.test.tsx
+++ src/pages/SelectMode/index.test.tsx
@@ -1,10 +1,16 @@
 import { render } from '@testing-library/react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import SelectMode from '.';
 
 describe('SelectMode', () => {
   it('버튼들이 보여집니다.', () => {
-    const { getByText } = render(<SelectMode />);
+    const { getByText } = render(
+      <MemoryRouter>
+        <SelectMode />
+      </MemoryRouter>
+    );
 
     expect(getByText('Focus')).not.toBeNull();
     expect(getByText('Break')).not.toBeNull();
