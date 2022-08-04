91c6376884c831c5371a0aac7dffce3a19a2f469
Woo Dy <kjop7419@naver.com>
2022-08-04 14:42:40 +0900

    feat: focus 페이지를 연결시켰습니다.

--- src/containers/SelectModeContainer/index.tsx
+++ src/containers/SelectModeContainer/index.tsx
@@ -1,7 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
 const SelectModeContainer = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = (path: string) => {
+    navigate(`${path}`);
+  };
+
   return (
     <>
-      <button>Focus</button>
+      <button onClick={() => handleNavigate('/focus')}>Focus</button>
       <button>Break</button>
     </>
   );
