3a39f786d1f5d124aff7b6d0a30baecee75b22cb
Woo Dy <kjop7419@naver.com>
2022-08-05 22:58:56 +0900

    리덕스를 통해 종료 시간을 정의하라
    setEndTime을 통해 종료 시간을 정의하는 작업을 진행하였습니다.

--- src/containers/TimeSelectContainer/index.tsx
+++ src/containers/TimeSelectContainer/index.tsx
@@ -1,10 +1,28 @@
-import { FC, Fragment } from 'react';
+import { FC, Fragment, useEffect } from 'react';
 
+import { setEndTime, timer } from 'slice/time';
+
+import { useDispatch } from 'react-redux';
 interface Times {
   times: string[];
 }
 
 const TimeSelectContainer: FC<Times> = ({ times }) => {
+  const dispatch = useDispatch();
+
+  const end = Math.floor(new Date().getTime() / 1000 + 1500);
+
+  useEffect(() => {
+    setInterval(() => {
+      const now = Math.floor(new Date().getTime() / 1000);
+      dispatch(timer(now));
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    dispatch(setEndTime(end));
+  }, []);
+
   return (
     <>
       {times.map((time, idx) => (
