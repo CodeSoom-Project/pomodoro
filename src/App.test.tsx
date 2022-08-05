79f711b3f79be215736b033b899db63c9099dd0e
Woo Dy <kjop7419@naver.com>
2022-08-05 22:59:50 +0900

    리덕스를 연결하는 테스트를 진행하라
    리덕스를 연결하여 테스트가 깨지던 부분을 수정하였습니다.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,19 @@ import { render } from '@testing-library/react';
 import { times } from 'common/times';
 import { MemoryRouter } from 'react-router';
 
+import { useDispatch } from 'react-redux';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 describe('App', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    (useDispatch as jest.Mock).mockImplementation(() => dispatch);
+  });
+
   const renderApp = (path: string) => {
     return render(
       <MemoryRouter initialEntries={[path]}>
