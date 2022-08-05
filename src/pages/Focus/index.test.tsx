51c2c65b99aa0e305c3f109d65a37d6524b47917
Woo Dy <kjop7419@naver.com>
2022-08-05 22:58:05 +0900

    리덕스를 사용하기 위해 테스트를 수정하라
    react-redux를 mocking하여 테스트가 깨지는 것을 방지하였습니다.

--- src/pages/Focus/index.test.tsx
+++ src/pages/Focus/index.test.tsx
@@ -1,11 +1,21 @@
 import { render } from '@testing-library/react';
 
+import { useDispatch } from 'react-redux';
+
 import { times } from 'common/times';
 import { MemoryRouter } from 'react-router-dom';
 
 import Focus from '.';
 
+jest.mock('react-redux');
+
 describe('Focus', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    (useDispatch as jest.Mock).mockImplementation(() => dispatch);
+  });
+
   it('시간 선택 버튼이 보여집니다.', () => {
     const { container } = render(
       <MemoryRouter>
