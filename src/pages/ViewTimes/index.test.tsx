13464632cbefdfab5eb0d222bf9aa9770c1c9cdf
Woo Dy <kjop7419@naver.com>
2022-08-07 01:57:01 +0900

    추가되는 페이지의 테스트를 작성하라
    ViewTimes의 테스트를 작성하였습니다.

---
+++ src/pages/ViewTimes/index.test.tsx
@@ -0,0 +1,38 @@
+import { render } from '@testing-library/react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { MemoryRouter } from 'react-router-dom';
+
+import ViewTimes from '.';
+import { RootState } from 'store/reducer';
+
+jest.mock('react-redux');
+
+describe('Break', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    (useDispatch as jest.Mock).mockImplementation(() => dispatch);
+
+    (useSelector as jest.Mock).mockImplementation(
+      (state: (arg: RootState) => void) =>
+        state({
+          time: {
+            endTime: 0,
+            remainTime: '00 : 00',
+          },
+        })
+    );
+  });
+
+  it('시간이 보여집니다.', () => {
+    const { container } = render(
+      <MemoryRouter>
+        <ViewTimes />
+      </MemoryRouter>
+    );
+
+    expect(container).toHaveTextContent('00 : 00');
+  });
+});
