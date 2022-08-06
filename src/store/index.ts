1acc4aa0f6a884feff9fb705c2b8c6cf6acdc992
Woo Dy <kjop7419@naver.com>
2022-08-07 01:50:01 +0900

    redux-logger를 삭제하라
    리덕스 로거가 불필요하여 개발 의존성에서 삭제하고
    리덕스 로거를 사용하지 않도록 store를 설정하였습니다.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import logger from 'redux-logger';
-
 import rootReducer from './reducer';
 
 const store = configureStore({
   reducer: rootReducer,
   middleware: getDefaultMiddleware => [
     ...getDefaultMiddleware(),
-    ...(process.env.NODE_ENV === 'development' ? [logger] : []),
+    ...(process.env.NODE_ENV === 'development' ? [] : []),
   ],
   devTools: process.env.NODE_ENV !== 'production',
 });
