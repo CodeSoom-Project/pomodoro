2289aa7f0a76870d2584423de1f0607775ae39df
Woo Dy <kjop7419@naver.com>
2022-08-08 16:10:37 +0900

    사용하지 않는 부분을 제거하고 대체하라
    type을 분리함으로 Times 인터페이스를 제거하고
    불필요한 조건문을 제거 후
    TimeButton을 활용하여 기존 button 태그를 제거하였습니다.

--- src/containers/TimeSelectContainer/index.tsx
+++ src/containers/TimeSelectContainer/index.tsx
@@ -1,26 +1,24 @@
+import TimeButton from 'components/TimeButton';
 import { FC, Fragment } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
+import { Times } from 'typings';
+
 import { setEnd } from 'utils';
-interface Times {
-  times: string[];
-}
 
 const TimeSelectContainer: FC<Times> = ({ times }) => {
   const navigate = useNavigate();
 
   const handleSetEnd = (seconds: string) => {
-    if (Number(seconds)) {
-      navigate('/viewtimes', { state: setEnd(seconds) });
-    }
+    navigate('/viewtimes', { state: setEnd(seconds) });
   };
 
   return (
     <>
       {times.map((time, idx) => (
         <Fragment key={idx}>
-          <button onClick={() => handleSetEnd(time)}>{time}</button>
+          <TimeButton time={time} onClick={handleSetEnd} />
         </Fragment>
       ))}
     </>
