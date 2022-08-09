948d06cfe0c65910408e384cca31275f78a3b685
Woo Dy <kjop7419@naver.com>
2022-08-09 17:59:43 +0900

    사용하지 않는 코드를 삭제하고 명확하지 않은 변수명을 변경하라
    get과 equal은 사용하지 않기 때문에 코드에서 삭제하였고
    setNow가 명확한 표현이 아니어서 currentTimeStampSeconds로 변경하였습니다.

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,6 @@
-export function get(key: any) {
-  return (obj: any) => obj[key];
-}
-
-export function equal(key: any, value: any) {
-  return (obj: any) => obj[key] === value;
-}
-
 export const convertToClock = (seconds: number): string => {
   if (seconds < 0) {
     return '00 : 00';
-  }
-
-  if (isNaN(seconds)) {
-    return 'Loading';
   }
 
   const remainSeconds = String(Math.floor(seconds % 60)).padStart(2, '0');
@@ -25,6 +13,6 @@ export const setEnd = (seconds: string): number => {
   return Math.floor(new Date().getTime() / 1000 + Number(seconds) * 60);
 };
 
-export const setNow = () => {
+export const currentTimestampSeconds = (): number => {
   return Math.floor(new Date().getTime() / 1000);
 };
