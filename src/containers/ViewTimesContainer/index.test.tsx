8bf345ab31b26b0decf31f57a45efcca3f64d572
Woo Dy <kjop7419@naver.com>
2022-08-13 02:24:32 +0900

    모든 유닛 테스트를 진행하라
    setInterval테스트를 진행함으로
    테스트 커버리지 100을 완료하였습니다.

--- src/containers/ViewTimesContainer/index.test.tsx
+++ src/containers/ViewTimesContainer/index.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -7,10 +7,20 @@ import { RootState } from 'store/reducer';
 
 import ViewTimesContainer from '.';
 
+import { currentTimestampSeconds } from 'utils';
+
 jest.mock('react-redux');
 
 describe('ViewTimesContainer', () => {
   const dispatch = jest.fn();
+
+  const renderViewTimeContainer = () => {
+    return render(
+      <MemoryRouter>
+        <ViewTimesContainer />
+      </MemoryRouter>
+    );
+  };
 
   beforeEach(() => {
     (useDispatch as jest.Mock).mockImplementation(() => dispatch);
@@ -20,19 +30,45 @@ describe('ViewTimesContainer', () => {
         state({
           time: {
             endTime: 0,
-            remainTime: '00 : 00',
+            remainTime: '01 : 00',
           },
         })
     );
   });
 
   it('시간이 보여집니다.', () => {
-    const { container } = render(
-      <MemoryRouter>
-        <ViewTimesContainer />
-      </MemoryRouter>
-    );
+    const { container } = renderViewTimeContainer();
 
-    expect(container).toHaveTextContent('00 : 00');
+    expect(container).toHaveTextContent('01 : 00');
+  });
+
+  it('디스패치가 호출됩니다.', () => {
+    renderViewTimeContainer();
+
+    expect(dispatch).toBeCalledWith({
+      payload: { currentTime: currentTimestampSeconds(), endTime: null },
+      type: 'time/setEndTime',
+    });
+  });
+
+  it('시간이 지나면 디스패치가 추가로 호출됩니다.', async () => {
+    renderViewTimeContainer();
+
+    await waitFor(
+      () =>
+        expect(dispatch).toBeCalledWith({
+          payload: { currentTime: currentTimestampSeconds(), endTime: null },
+          type: 'time/setEndTime',
+        }),
+      { timeout: 2000 }
+    );
+    await waitFor(
+      () =>
+        expect(dispatch).toBeCalledWith({
+          payload: { currentTime: currentTimestampSeconds() },
+          type: 'time/timer',
+        }),
+      { timeout: 2000 }
+    );
   });
 });
