fbffaca9a6954418827af00303888f666df354d7
Woo Dy <kjop7419@naver.com>
2022-08-14 20:29:57 +0900

    스타일링을 하라

--- src/components/TimeButton/index.tsx
+++ src/components/TimeButton/index.tsx
@@ -1,10 +1,34 @@
 import { FC } from 'react';
+
+import styled from '@emotion/styled';
+
 import { SelectableTime } from 'typings';
+
+const Button = styled.button({
+  width: '5em',
+
+  margin: '1rem',
+  padding: '2em 0',
+
+  border: '6px solid #EDDFB3',
+  borderRadius: '50%',
+
+  fontSize: '1rem',
+  textAlign: 'center',
+  wordBreak: 'break-all',
+
+  color: '#CA955C',
+
+  '&:hover': {
+    fontSize: '1.5rem',
+    color: '#876445',
+  },
+});
 
 const TimeButton: FC<SelectableTime> = ({ time, onClick }) => {
   return (
     <>
-      <button onClick={() => onClick(time)}>{time}</button>
+      <Button onClick={() => onClick(time)}>{time}</Button>
     </>
   );
 };
