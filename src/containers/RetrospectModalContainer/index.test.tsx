c982d73dd51a23b3e5903fec37aa73ba5389dd36
Woo Dy <kjop7419@naver.com>
2022-08-15 00:30:25 +0900

    모달에 retrospect가 inmemory로 저장되게 하라

--- src/containers/RetrospectModalContainer/index.test.tsx
+++ src/containers/RetrospectModalContainer/index.test.tsx
@@ -1,14 +1,46 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+
+import { useDispatch } from 'react-redux';
+import { MemoryRouter } from 'react-router';
 
 import RetrospectModalContiner from '.';
 
 describe('RetrospectModalContainer', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    (useDispatch as jest.Mock).mockImplementation(() => dispatch);
+  });
+
   const renderRetrospectModalContainer = () => {
-    return render(<RetrospectModalContiner />);
+    return render(
+      <MemoryRouter>
+        <RetrospectModalContiner />
+      </MemoryRouter>
+    );
   };
   it('헤더가 나타납니다.', () => {
     const { container } = renderRetrospectModalContainer();
 
     expect(container).toHaveTextContent('집중 시간동안 어떤 것을 하셨나요?');
+  });
+
+  it('textArea에 "회고"를 입력하면 value가 회고가됩니다.', () => {
+    const { getByRole, container } = renderRetrospectModalContainer();
+
+    fireEvent.change(getByRole('textbox'), { target: { value: '회고' } });
+
+    expect(container).toHaveTextContent('회고');
+  });
+
+  it('작성하기 버튼을 클릭하면 dispatch가 호출됩니다.', () => {
+    const { getByText } = renderRetrospectModalContainer();
+
+    fireEvent.click(getByText('작성하기'));
+
+    expect(dispatch).toBeCalledWith({
+      type: 'retrospect/addRetrospect',
+      payload: { contents: '', id: 0 },
+    });
   });
 });
--- src/containers/RetrospectModalContainer/index.tsx
+++ src/containers/RetrospectModalContainer/index.tsx
@@ -1,11 +1,59 @@
+import { FormEvent, useCallback, useState } from 'react';
+
+import styled from '@emotion/styled';
+
 import RetrospectModal from 'components/RetrospectModal';
 
+import { useDispatch } from 'react-redux';
+import { addRetrospect } from 'slice/retrospect';
+
+const CenterLayout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 40em;
+  margin: 20rem auto;
+  padding: 3rem;
+
+  border: 2px solid black;
+  border-radius: 2rem;
+  z-index: 4;
+`;
+
 const RetrospectModalContiner = () => {
+  const dispatch = useDispatch();
+
+  const [retrospect, setRetrospect] = useState({
+    id: 0,
+    contents: '',
+  });
+
+  const handleChangeRetrospect = useCallback(
+    (event: FormEvent<HTMLInputElement>) => {
+      setRetrospect({
+        ...retrospect,
+        contents: event.currentTarget.value,
+      });
+    },
+    [setRetrospect]
+  );
+
+  const handleSubmitRetrospect = useCallback(() => {
+    dispatch(addRetrospect(retrospect));
+  }, [dispatch, retrospect]);
+
   return (
-    <>
-      <h1>집중 시간동안 어떤 것을 하셨나요?</h1>
-      <RetrospectModal />
-    </>
+    <CenterLayout>
+      <div>
+        <h1>집중 시간동안 어떤 것을 하셨나요?</h1>
+        <RetrospectModal
+          onChangeRetrospect={handleChangeRetrospect}
+          submitRetrospect={handleSubmitRetrospect}
+          retrospect={retrospect}
+        />
+      </div>
+    </CenterLayout>
   );
 };
 
