524759aea3adcd303c8a38c462bc65909ded4cee
Woo Dy <kjop7419@naver.com>
2022-08-16 00:02:39 +0900

    endPomodoro를 정의하고 테스트하라

--- src/components/Time/index.test.tsx
+++ src/components/Time/index.test.tsx
@@ -1,11 +1,25 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Time from '.';
 
 describe('Time', () => {
+  const endPomodoro = jest.fn();
+
+  const renderTime = () => {
+    return render(<Time remainTime="14 : 33" endPomodoro={endPomodoro} />);
+  };
+
   it('전달받은 시간이 렌더링 됩니다.', () => {
-    const { container } = render(<Time remainTime="14 : 33" />);
+    const { container } = renderTime();
 
     expect(container).toHaveTextContent('14 : 33');
+  });
+
+  it('종료 버튼을 누르면 endPomodoro가 호출됩니다.', () => {
+    const { getByText } = renderTime();
+
+    fireEvent.click(getByText('종료'));
+
+    expect(endPomodoro).toBeCalled();
   });
 });
--- src/components/Time/index.tsx
+++ src/components/Time/index.tsx
@@ -2,8 +2,13 @@ import { FC } from 'react';
 
 import { RemainTime } from 'typings';
 
-const Time: FC<RemainTime> = ({ remainTime }) => {
-  return <div>{remainTime}</div>;
+const Time: FC<RemainTime> = ({ remainTime, endPomodoro }) => {
+  return (
+    <>
+      <div>{remainTime}</div>
+      <button onClick={endPomodoro}>종료</button>
+    </>
+  );
 };
 
 export default Time;
