9ef9673d20c81d9430053ac5dc0032c279af318b
Woo Dy <kjop7419@naver.com>
2022-08-16 00:10:37 +0900

    Retrospect 페이지를 정의하라

---
+++ src/pages/Retrospect/index.tsx
@@ -0,0 +1,11 @@
+import RetrospectContainer from 'containers/RetrospectContainer';
+
+const Retrospect = () => {
+  return (
+    <>
+      <RetrospectContainer />
+    </>
+  );
+};
+
+export default Retrospect;
