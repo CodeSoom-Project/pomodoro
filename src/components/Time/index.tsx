d4479e53d374cd52852a0987c94b46ce49a3e6f5
Woo Dy <kjop7419@naver.com>
2022-08-17 19:42:21 +0900

    일시정지 기능과 재개 기능을 구현하라

--- src/components/Time/index.tsx
+++ src/components/Time/index.tsx
@@ -2,11 +2,19 @@ import { FC } from 'react';
 
 import { RemainTime } from 'typings';
 
-const Time: FC<RemainTime> = ({ remainTime, endPomodoro }) => {
+const Time: FC<RemainTime> = ({
+  remainTime,
+  endPomodoro,
+  isPause,
+  pause,
+  resume,
+}) => {
   return (
     <>
       <div>{remainTime}</div>
-      <button onClick={endPomodoro}>종료</button>
+      <button onClick={endPomodoro}>일정 종료</button>
+      {!isPause && <button onClick={pause}>일시 정지</button>}
+      {isPause && <button onClick={resume}>재개</button>}
     </>
   );
 };
