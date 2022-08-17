6c203e253bbd6b4f8351f568aa7d36d983e064a9
Woo Dy <kjop7419@naver.com>
2022-08-17 19:40:16 +0900

    일시정지 기능 및 재개 기능을 구현하라
    일시정지 기능과 재개 기능을 구현하였습니다.

--- src/containers/ViewTimesContainer/index.test.tsx
+++ src/containers/ViewTimesContainer/index.test.tsx
@@ -5,11 +5,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
 import { RootState } from 'store/reducer';
 
+import { initialState } from 'slice/time';
+import { retroSpectInitialState } from 'slice/retrospect';
+
 import ViewTimesContainer from '.';
 
 import { currentTimestampSeconds } from 'utils';
 
-import { Status } from 'typings/time';
+import { Mode, Status } from 'typings/time';
 
 jest.mock('react-redux');
 
@@ -38,15 +41,10 @@ describe('ViewTimesContainer', () => {
       (state: (arg: RootState) => void) =>
         state({
           time: {
-            endTime: 0,
+            ...initialState,
             remainTime: '01 : 00',
-            location: '/focus',
-            status: Status.Initial,
           },
-          retrospect: {
-            isEnd: false,
-            retrospect: [],
-          },
+          retrospect: retroSpectInitialState,
         })
     );
   });
@@ -84,33 +82,132 @@ describe('ViewTimesContainer', () => {
     );
   });
 
-  it('isEnd가 True고 location이 "/focus"면 모달이 나타납니다.', () => {
+  describe('isEnd 상태이며 mode가 Focus일 때', () => {
+    it('모달이 나타납니다.', () => {
+      (useSelector as jest.Mock).mockImplementation(
+        (state: (arg: RootState) => void) =>
+          state({
+            time: {
+              ...initialState,
+              isPause: true,
+              status: Status.End,
+            },
+            retrospect: {
+              ...retroSpectInitialState,
+              isEnd: true,
+            },
+          })
+      );
+      const { container } = renderViewTimeContainer();
+
+      expect(container).toHaveTextContent('작성하기');
+    });
+  });
+
+  describe('isEnd 상태이며 mode가 Break일 때', () => {
+    it('dispatch가 호출되고 navigate가 됩니다.', () => {
+      (useSelector as jest.Mock).mockImplementation(
+        (state: (arg: RootState) => void) =>
+          state({
+            time: {
+              ...initialState,
+              mode: Mode.Break,
+              status: Status.End,
+              isPause: true,
+            },
+            retrospect: {
+              ...retroSpectInitialState,
+              isEnd: true,
+            },
+          })
+      );
+      renderViewTimeContainer();
+
+      expect(dispatch).toBeCalledWith({
+        payload: Mode.Focus,
+        type: 'time/setMode',
+      });
+    });
+  });
+
+  it('일정 종료 버튼을 누르면 endPomodoro가 호출됩니다.', () => {
+    const { getByText } = renderViewTimeContainer();
+
+    fireEvent.click(getByText('일정 종료'));
+
+    expect(mockedUsedNavigate).toHaveBeenCalledTimes(1);
+  });
+
+  it('일시 정지 버튼을 누르면 pause가 디스패치와 함께 호출됩니다.', () => {
+    const { getByText } = renderViewTimeContainer();
+
+    fireEvent.click(getByText('일시 정지'));
+
+    expect(dispatch).toBeCalledWith({
+      payload: currentTimestampSeconds(),
+      type: 'time/setPause',
+    });
+  });
+
+  it('재개 버튼을 누르면 resume이 디스패치와 함께 호출됩니다.', async () => {
     (useSelector as jest.Mock).mockImplementation(
       (state: (arg: RootState) => void) =>
         state({
           time: {
-            endTime: 0,
-            remainTime: '00 : 00',
-            location: '/focus',
+            ...initialState,
             status: Status.End,
+            isPause: true,
           },
           retrospect: {
+            ...retroSpectInitialState,
             isEnd: true,
-            retrospect: [],
           },
         })
     );
 
-    const { container } = renderViewTimeContainer();
-
-    expect(container).toHaveTextContent('작성하기');
-  });
-
-  it('종료 버튼을 누르면 endPomodoro가 호출됩니다.', () => {
     const { getByText } = renderViewTimeContainer();
 
-    fireEvent.click(getByText('종료'));
+    fireEvent.click(getByText('재개'));
 
-    expect(mockedUsedNavigate).toHaveBeenCalledTimes(1);
+    expect(dispatch).toBeCalledWith({
+      payload: Status.Resume,
+      type: 'time/setStatus',
+    });
+  });
+
+  it('Status가 resume일 때', async () => {
+    (useSelector as jest.Mock).mockImplementation(
+      (state: (arg: RootState) => void) =>
+        state({
+          time: {
+            ...initialState,
+            remainTime: '02 : 00',
+            status: Status.Resume,
+          },
+          retrospect: {
+            ...retroSpectInitialState,
+          },
+        })
+    );
+    renderViewTimeContainer();
+
+    expect(dispatch).toBeCalledWith({
+      payload: { currentTime: currentTimestampSeconds(), endTime: null },
+      type: 'time/setEndTime',
+    });
+
+    expect(dispatch).toBeCalledWith({
+      payload: Status.Running,
+      type: 'time/setStatus',
+    });
+
+    await waitFor(
+      () =>
+        expect(dispatch).toBeCalledWith({
+          payload: { currentTime: currentTimestampSeconds() },
+          type: 'time/timer',
+        }),
+      { timeout: 2000 }
+    );
   });
 });
