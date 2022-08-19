571b7a8684a04f6757c36a68f323700675891608
Woo Dy <kjop7419@naver.com>
2022-08-20 00:09:54 +0900

    유저가 시간을 추가할 수 있게 하라
    유저가 시간을 추가할 수 있게끔 setTimes 디스패치를 연결하였습니다.

--- src/containers/TimeSelectContainer/index.tsx
+++ src/containers/TimeSelectContainer/index.tsx
@@ -1,24 +1,35 @@
-import { FC, Fragment } from 'react';
+import React, { FC, Fragment, useCallback, useState } from 'react';
+
+import styled from '@emotion/styled';
 
 import { useNavigate, useLocation } from 'react-router-dom';
 
-import { useDispatch } from 'react-redux';
-import { setMode } from 'slice/time';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMode, setTimes } from 'slice/time';
 
 import { setEnd } from 'utils';
 
 import TimeButton from 'components/TimeButton';
 
-import { Times } from 'typings';
 import { Mode } from 'typings/time';
+import TimeAddForm from 'components/TimeAddForm';
+import { RootState } from 'store/reducer';
 
-const TimeSelectContainer: FC<Times> = ({ times }) => {
+const Wrapper = styled.div`
+  max-width: 25rem;
+`;
+
+const TimeSelectContainer = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const [addTime, setAddTime] = useState('');
 
   const { pathname } = useLocation();
 
   const isFocus = pathname === Mode.Focus;
+
+  const { times } = useSelector((state: RootState) => state.time);
 
   const navigateHandler = () => {
     if (pathname === Mode.Focus) {
@@ -42,15 +53,33 @@ const TimeSelectContainer: FC<Times> = ({ times }) => {
     navigate('/viewtimes', { state: setEnd(selectedTime) });
   };
 
+  const timeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+
+    const regex = /^[0-9\b]{0,13}$/;
+    if (regex.test(value)) {
+      setAddTime(value);
+    }
+  };
+
+  const addTimeHandler = useCallback((time: string) => {
+    dispatch(setTimes(time));
+  }, []);
+
   return (
-    <div style={{ maxWidth: '25rem' }}>
+    <Wrapper>
+      <TimeAddForm
+        onClick={addTimeHandler}
+        addTime={addTime}
+        onChange={timeHandler}
+      />
       {times.map((time, idx) => (
         <Fragment key={idx}>
           <TimeButton time={time} onClick={handleSetEnd} />
         </Fragment>
       ))}
       <TimeButton time={isFocus ? 'Break' : 'Focus'} onClick={handleSetEnd} />
-    </div>
+    </Wrapper>
   );
 };
 
