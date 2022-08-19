1d2130bcb261493dc5f71354d6a653b101d626b1
Woo Dy <kjop7419@naver.com>
2022-08-19 10:44:03 +0900

    Ui가 깨지는 것을 수정하라

--- src/styles/Layouts/index.tsx
+++ src/styles/Layouts/index.tsx
@@ -6,7 +6,9 @@ import { Layout } from 'typings/styles';
 
 const LayoutStyle = styled.div<{ background: string }>`
   width: 100%;
-  height: 100vh;
+  height: 100%;
+  min-height: 100vh;
+  padding-bottom: 12rem;
   background: ${props => props.background};
   text-align: center;
 `;
