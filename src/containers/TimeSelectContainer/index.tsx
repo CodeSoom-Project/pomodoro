2e02e32f5533ea7300fb562c8573c2cea7f83fa6
Woo Dy <kjop7419@naver.com>
2022-09-26 14:31:52 +0900

    css를 수정하라

--- src/containers/TimeSelectContainer/index.tsx
+++ src/containers/TimeSelectContainer/index.tsx
@@ -11,13 +11,109 @@ import { setMode, setTimes } from 'slice/time';
 
 import { setEnd } from 'utils';
 
-import TimeAddForm from 'components/TimeAddForm';
 import TimeButton from 'components/TimeButton';
+import TimeAddForm from 'components/TimeAddForm';
 
 import { Mode } from 'typings/time';
 
 const Wrapper = styled.div`
   max-width: 25rem;
+`;
+
+const ButtonWrapper = styled.div`
+  margin-top: 3rem;
+
+  & :first-of-type {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 4rem;
+    height: 4rem;
+  }
+
+  & :nth-of-type(2) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 5rem;
+    height: 5rem;
+
+    margin-top: -5rem;
+    margin-left: 5rem;
+
+    font-size: 1.5rem;
+  }
+
+  & :nth-of-type(3) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 6rem;
+    height: 6rem;
+
+    margin-top: -2rem;
+
+    font-size: 1.5rem;
+  }
+
+  & :nth-of-type(4) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 7rem;
+    height: 7rem;
+
+    margin-top: -14rem;
+    margin-left: 10rem;
+
+    font-size: 1.5rem;
+  }
+
+  & :nth-of-type(5) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 9rem;
+    height: 9rem;
+
+    margin-left: 5em;
+
+    font-size: 1.5rem;
+  }
+
+  & :nth-of-type(6) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 9rem;
+    height: 9rem;
+
+    margin-top: -3rem;
+    margin-left: 0rem;
+
+    font-size: 1.5rem;
+  }
+
+  & :nth-of-type(7) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 10rem;
+    height: 10rem;
+
+    margin-top: -6rem;
+    margin-left: 9rem;
+
+    font-size: 2rem;
+  }
 `;
 
 const TimeSelectContainer = () => {
@@ -75,13 +171,13 @@ const TimeSelectContainer = () => {
         addTime={addTime}
         onChange={timeHandler}
       />
-      <>
+      <ButtonWrapper>
         {times.map((time, idx) => (
           <Fragment key={idx}>
             <TimeButton time={time} onClick={handleSetEnd} />
           </Fragment>
         ))}
-      </>
+      </ButtonWrapper>
       <TimeButton time={isFocus ? 'Break' : 'Focus'} onClick={handleSetEnd} />
     </Wrapper>
   );
